29ae14ee30b011fc59ccc3c8040a3065369d0a47
felichri <felichri@umich.edu>
2018-02-10 20:15:56 -0500

    Images

--- glitch_photos.sh
+++ glitch_photos.sh
@@ -5,5 +5,10 @@ git pull
 for f in *.jpg; do
 byebyte destroy -i "$f" -o "$f" --min 0.1 --max 0.9 -t 1000;
 done
+for f in *.JPG; do
+byebyte destroy -i "$f" -o "$f" --min 0.1 --max 0.9 -t 1000;
+done
+git add .
+git commit -m "Images"
 git push
 
